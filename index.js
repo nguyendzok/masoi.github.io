b7a825570aa81546be33191e0c203cc184ce3561
Pham Ngoc Duy <phamngocduy98@gmail.com>
2018-07-29 18:44:41 +0700

    updates and fixes

--- index.js
+++ index.js
@@ -44,25 +44,48 @@ async function roomRoleChat(roomID) {
         let villagersList = gamef.getRoom(roomID).villagersTxt.join(' ; ');
         let playersList = gamef.getRoom(roomID).playersTxt.join(' ; ');
         if (m.role == -1) {//SÓI
-          bot.say(m.joinID, `🐺Sói ơi dậy đi! Đêm nay sói muốn cắn ai?\n/vote <id> để cắn 1 ai đó\n👨‍👩‍👦‍👦ID CẢ LÀNG:\n${playersList}\n🐺ID TEAM SÓI:\n${wolfList}\n💩ID TEAM DÂN:\n${villagersList}`);
+          bot.say(m.joinID, [{
+            attachment: 'image',
+            url: 'http://hstatic.net/936/1000019936/10/2015/7-28/masoi.jpg'
+          }, `🐺Sói ơi dậy đi! Đêm nay sói muốn cắn ai?\n/vote <id> để cắn 1 ai đó\n👨‍👩‍👦‍👦ID CẢ LÀNG:\n${playersList}\n🐺ID TEAM SÓI:\n${wolfList}\n💩ID TEAM DÂN:\n${villagersList}`]);
         } else if (m.role == 1) { // tiên tri
-          bot.say(m.joinID, `🔍Tiên tri dậy đi! Tiên tri muốn kiểm tra ai?\n/see <id> để kiểm tra\n${playersList}`);
+          bot.say(m.joinID, [{
+            attachment: 'image',
+            url: 'http://hstatic.net/936/1000019936/10/2015/11-18/tien-tri.jpg'
+          }, `🔍Tiên tri dậy đi! Tiên tri muốn kiểm tra ai?\n/see <id> để kiểm tra\n${playersList}`]);
         } else if (m.role == 2) { // Bảo vệ
-          bot.say(m.joinID, `🗿Bảo vệ dậy đi! Đêm nay bạn muốn bảo vệ ai?\n/save <id> để bảo vệ\n${playersList}`);
+          bot.say(m.joinID, [{
+            attachment: 'image',
+            url: 'http://hstatic.net/936/1000019936/10/2015/7-28/baove.jpg'
+          }, `🗿Bảo vệ dậy đi! Đêm nay bạn muốn bảo vệ ai?\n/save <id> để bảo vệ\n${playersList}`]);
         } else if (m.role == 3) { // Thợ săn
-          bot.say(m.joinID, `🔫Thợ săn dậy đi! Đêm nay bạn muốn bắn ai?\n/fire <id> để ngắm bắn\n${playersList}`);
+          bot.say(m.joinID, [{
+            attachment: 'image',
+            url: 'http://hstatic.net/936/1000019936/10/2015/7-28/thosan.jpg'
+          }, `🔫Thợ săn dậy đi! Đêm nay bạn muốn bắn ai?\n/fire <id> để ngắm bắn\n${playersList}`]);
         } else if (m.role == 4) { // Bán sói
-          bot.say(m.joinID, `🐺Bạn là BÁN SÓI!\nBạn vẫn còn là DÂN! Ngủ tiếp đi!\nID CẢ LÀNG:\n${playersList}`);
+          bot.say(m.joinID, [{
+            attachment: 'image',
+            url: 'http://hstatic.net/936/1000019936/10/2015/7-28/phanboi.jpg'
+          }, `🐺Bạn là BÁN SÓI!\nBạn vẫn còn là DÂN! Ngủ tiếp đi!\nID CẢ LÀNG:\n${playersList}`]);
           gamef.getRoom(roomID).roleDoneBy(m.joinID);
         } else if (m.role == 5) { // Phù thủy
+          let sayTxt;
           if (gamef.getRoom(roomID).witchKillRemain) {
-            bot.say(m.joinID, `🔮Bạn là Phù thủy!\n${gamef.getRoom(roomID).witchSaveRemain ? '☑Bạn còn quyền cứu' : '⛔Bạn đã dùng quyền cứu!'}\n☑/kill <id> để giết\n☑/skip để bỏ qua\n${playersList}`);
+            sayTxt = `🔮Bạn là Phù thủy!\n${gamef.getRoom(roomID).witchSaveRemain ? '☑Bạn còn quyền cứu' : '⛔Bạn đã dùng quyền cứu!'}\n☑/kill <id> để giết\n☑/skip để bỏ qua\n${playersList}`;
           } else {
-            bot.say(m.joinID, `🔮Bạn là Phù thủy!\n${gamef.getRoom(roomID).witchSaveRemain ? '☑Bạn còn quyền cứu' : '⛔Bạn đã dùng quyền cứu!'}\n⛔Bạn đã dùng quyền giết!\n${playersList}`);
+            sayTxt = `🔮Bạn là Phù thủy!\n${gamef.getRoom(roomID).witchSaveRemain ? '☑Bạn còn quyền cứu' : '⛔Bạn đã dùng quyền cứu!'}\n⛔Bạn đã dùng quyền giết!\n${playersList}`;
             gamef.getRoom(roomID).roleDoneBy(m.joinID);
           }
+          bot.say(m.joinID, [{
+            attachment: 'image',
+            url: 'http://hstatic.net/936/1000019936/10/2015/7-28/phuthuy.jpg'
+          }, sayTxt]);
         } else {
-          bot.say(m.joinID, `💩Bạn là DÂN! Ngủ tiếp đi :))\n👨‍👩‍👦‍👦ID CẢ LÀNG:\n${playersList}`);
+          bot.say(m.joinID, [{
+            attachment: 'image',
+            url: 'http://hstatic.net/936/1000019936/10/2015/7-28/danlang.jpg'
+          }, `💩Bạn là DÂN! Ngủ tiếp đi :))\n👨‍👩‍👦‍👦ID CẢ LÀNG:\n${playersList}`]);
           gamef.getRoom(roomID).roleDoneBy(m.joinID);
         }
       }
@@ -601,12 +624,10 @@ bot.on('attachment', (payload, chat) => {
       roomChatAll(userRoom, joinID, `*${user.first_name}* đã gửi nội dung không được hỗ trợ!`);
     }
   } else {
-    chat.sendAttachment('image', img.payload.url);
-    chat.say()
     if (userRoom != undefined) {
       console.log(`$ ROOM ${userRoom + 1} CHAT > ${joinID}: IMAGE content`);
       let user = gamef.getRoom(userRoom).getPlayer(joinID);
-      roomChatAll(userRoom, joinID, [`*${user.first_name}* đã gửi 1 sticker/ảnh/gif!`, {
+      roomChatAll(userRoom, joinID, [`*${user.first_name}* đã gửi 1 sticker/ảnh/gif ...`, {
         attachment: 'image',
         url: img.payload.url
       }]);
