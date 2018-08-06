1f520d8fb5ff0de7062b2448fb0301d887ba4ede
Pham Ngoc Duy <phamngocduy98@gmail.com>
2018-08-06 16:14:25 +0700

    updates and fixes

--- src/Room/Join.js
+++ src/Room/Join.js
@@ -16,7 +16,7 @@ module.exports = (gamef, bot) => {
 
         const askRoom = (convo) => {
             convo.ask({
-                text: enableGreetingTxt?'Cảm ơn bạn đã tham gia chơi thử nghiệm Quản trò Ma sói Bot!\nBot vẫn hiện đang phát triển\nMọi lỗi phát sinh vui lòng comment trên fanpage để được fix sớm nhất có thể!\n\nVui lòng lựa chọn phòng:':'',
+                text: enableGreetingTxt?'Cảm ơn bạn đã tham gia chơi thử nghiệm Quản trò Ma sói Bot!\nBot vẫn hiện đang phát triển\nMọi lỗi phát sinh vui lòng comment trên fanpage để được fix sớm nhất có thể!\n\nVui lòng lựa chọn phòng:':'Chọn phòng:',
                 quickReplies: roomListView,
             }, (payload, convo) => {  
                 if (payload.message && payload.message.text.match(/\<|\>/g)) {
